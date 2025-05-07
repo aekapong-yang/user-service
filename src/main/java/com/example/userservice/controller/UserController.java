083a1201c269ea4fc6ed0da1d2883aa4b71882a5
Aekapong Yang <78343330+aekapong-yang@users.noreply.github.com>
2025-05-07 23:33:40 +0700

    Develop (#2)
    * add .gitignore
    
    * add user controller

---
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+
+    @GetMapping
+    public String getUserAll() {
+        return "get users all";
+    }
+
+    @GetMapping("/{id}")
+    public String getUserById(@PathVariable long id) {
+        return "users " + id;
+    }
+}
