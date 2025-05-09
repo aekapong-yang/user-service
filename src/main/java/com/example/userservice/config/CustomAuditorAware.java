94f533ab2ae426cc872263564a3e03d8da6dadfe
Aekapong <Aekapong.saeya@gmail.com>
2025-05-10 02:22:41 +0700

    add auditConfig

---
+++ src/main/java/com/example/userservice/config/CustomAuditorAware.java
@@ -0,0 +1,22 @@
+package com.example.userservice.config;
+
+import com.example.userservice.dto.ApiContext;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuditorAware implements AuditorAware<String> {
+    private final ApiContext apiContext;
+
+    @NonNull
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.ofNullable(apiContext.getUserId())
+                .or(() -> Optional.of("SYSTEM"));
+    }
+}
